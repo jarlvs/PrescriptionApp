6fea8cb017427c51337b160046b7786ef483dcd7
jarlvs <premshinde13@gmail.com>
2018-06-13 14:50:24 +0530

    Navigation from one activity to another

---
+++ app/src/main/java/com/example/prem/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.prem.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        startActivity(intent);
+    }
+}
